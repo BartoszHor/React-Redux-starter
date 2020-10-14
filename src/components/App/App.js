3f5054cfe5854c4634230bc5fdecb658516e830e
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-10-14 15:12:58 +0200

    Add animation while switching between nav links

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import Faq from '../Faq/Faq';
-
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {AnimatedSwitch} from 'react-router-transition';
+import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
+import styles from './App.scss';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-	  <Switch>
+	  <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
 	    <Route exact path='/' component={Home} />
 	    <Route exact path='/info' component={Info} />
 	    <Route exact path='/FAQ' component={Faq} />
-	  </Switch>
+	  </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
   
